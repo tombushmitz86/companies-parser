1059249e7de63b14f51c603474689b24f720ea69
tombushmitz86 <tombushmitz@gmail.com>
2020-08-22 10:52:11 +0300

    saving europian countries plot

--- companies.py
+++ companies.py
@@ -8,6 +8,10 @@ import requests
 from pandas.io.json import json_normalize
 import pandas as pd
 from tqdm import tqdm
+from countrygroups import EUROPEAN_UNION
+from collections import Counter
+import matplotlib.pyplot as plt
+
 
 COMPANIES_ENDPOINT = "https://pdl-canonical-data.s3-us-west-2.amazonaws.com/person/v10.0/company_v10.0_full.json"
 COMPANY_SIZE_ENUM_ENDPOINT = "https://pdl-canonical-data.s3-us-west-2.amazonaws.com/person/v10.0/company_size.txt"
@@ -24,6 +28,7 @@ def _get_company_size_range(company_size: int, company_size_ranges: List[Tuple[i
 
 def main(args=None):
     # Populate ENUM
+    EUROPEAN_UNION_COUNTRIES = [x.lower() for x in EUROPEAN_UNION.names]
     logger.info('fetching company size enum')
     response = request.urlopen(COMPANY_SIZE_ENUM_ENDPOINT)
     ranges = []
@@ -50,10 +55,19 @@ def main(args=None):
             pbar.set_description(f'Fetching entry {iterator_count}')
             company_size = _get_company_size_range(int(obj['employee_counts']['v9_total']), ranges)  
             obj['company_size_range'] = company_size
+            obj['is_europian'] =  obj['country'] in EUROPEAN_UNION_COUNTRIES
             entries.append(obj)
             iterator_count += 1
+            
             pbar.update(1)
+    dataframe = pd.read_json(json.dumps(entries))
+    only_europian = dataframe[dataframe['is_europian'] == True]
     
+    only_europian.groupby('country')['country'].count().plot(title='companies per country', kind='bar')
+    plt.savefig('plot.png')
+
+
+
     
 if __name__ == "__main__":
     logging.basicConfig()
